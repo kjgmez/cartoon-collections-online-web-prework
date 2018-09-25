9ccc14e60bed9ff0119c8140489dede1ef1b58cb
Kevin J Gomez <kjgmez1@gmail.com>
2018-09-25 01:05:41 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,7 +3,7 @@ def roll_call_dwarves(ar)
 end
 
 def summon_captain_planet(ar)
-  ar.collect {|x| x.upcase + "!"}
+  ar.collect {|x| x.capitalize + "!"}
 end
 
 def long_planeteer_calls# code an argument here
