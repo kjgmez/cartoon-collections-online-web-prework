31bc7dcd38116f4a5a7ed6789ebc13bd76f62dca
Kevin J Gomez <kjgmez1@gmail.com>
2018-09-25 01:21:22 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,5 +12,5 @@ end
 
 def find_the_cheese(ar) 
   cheese_types = ["cheddar", "gouda", "camembert"]
-  ar.find {|x| x == cheese_types[0] || cheese_types[1] || cheese_types[2]}
+  ar.find {|x| x == cheese_types}
 end
