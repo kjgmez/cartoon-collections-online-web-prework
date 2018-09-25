27935ed6776f35eb856f1bb8b68ebc9138d1b716
Kevin J Gomez <kjgmez1@gmail.com>
2018-09-25 00:51:51 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,5 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(ar)
+  ar.each_with_index {|x,i| puts "#{i + 1}. #{x}"}
 end
 
 def summon_captain_planet# code an argument here
