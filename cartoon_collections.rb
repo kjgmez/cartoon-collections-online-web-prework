f397508d3bf050ab5feb8a0d93157d8309e9f94f
Kevin J Gomez <kjgmez1@gmail.com>
2018-09-25 00:57:31 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,7 +2,7 @@ def roll_call_dwarves(ar)
   ar.each_with_index {|x,i| puts "#{i + 1}. #{x}"}
 end
 
-def summon_captain_planeta(ar)
+def summon_captain_planet(ar)
   ar.collect {|x| x.upcase << "!"}
 end
 
